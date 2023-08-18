eff2d8839e0b27d8fa59c57efc61d401f5d3c4f1
jaamong <amypure225@naver.com>
2023-08-18 18:04:15 +0900

    [Test] Redis 적용 확인 테스트

---
+++ src/test/java/com/jaamong/todo/RedisTest.java
@@ -0,0 +1,30 @@
+package com.jaamong.todo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class RedisTest {
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    void testStrings() {
+
+        //given
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String key = "accessToken";
+
+        //when
+        valueOperations.set(key, "test");
+
+        //then
+        String value = valueOperations.get(key);
+        Assertions.assertThat(value).isEqualTo("test");
+    }
+}
