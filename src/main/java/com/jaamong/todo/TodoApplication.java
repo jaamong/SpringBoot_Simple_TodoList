138f90b332c4e4da6ef72abf698c39c4ef38a28d
jaamong <amypure225@naver.com>
2023-08-19 17:35:05 +0900

    [Update] TodoApplication.java - @EnableJpaRepository 적용

--- src/main/java/com/jaamong/todo/TodoApplication.java
+++ src/main/java/com/jaamong/todo/TodoApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@EnableJpaRepositories(basePackages = {"com.jaamong.todo.repository"})
 @SpringBootApplication
 public class TodoApplication {
 
